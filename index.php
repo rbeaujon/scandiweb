0a154c10af84714fa4323a9a0c98ce9e57ee07fe
RBEAUJON <RBEAUJON77@GMAIL.COM>
2021-02-09 15:24:46 +0200

    menu.php  working from index

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<?PHP
+  <?php include 'client/views/menu.php'; ?>
 
-include 'server/session.php';
-
-?>
-
-
-<html>
-    <title>Scandiweb Test </title>
-<head>
-    <script src="client/scripts/master.js" type="text/javascript"></script>
-    <script src="https://code.jquery.com/jquery-latest.js"></script>
-    <link href="client/styles/main.css" rel="stylesheet" type="text/css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-    <div id="title" class="title title2">Product List </div>
-      
-      <div class="title">
-          <button type="button" id="add" onclick="add()" class="button add"> ADD </button>
-          <button type="button" id="massDelete" onclick="massdelete()" class="button delete"> MASS DELETE </button>
-      </div>  
-   
   <div class="line"></div> <!--  line -->
  
   <div id="formAdd" class = "hidden">
