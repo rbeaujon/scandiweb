e351dd454d3caef1de568591695f48caf0376cea
RBEAUJON <RBEAUJON77@GMAIL.COM>
2021-02-16 21:08:55 +0200

    Head fixed

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<html>
+    <title>Scandiweb Test </title>
+<head>
+    <script src="client/scripts/master.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
+    <link href="client/styles/main.css" rel="stylesheet" type="text/css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  
+  
   <?php include 'client/views/menu.php'; ?>
 
   <div class="line"></div> <!--  line -->
